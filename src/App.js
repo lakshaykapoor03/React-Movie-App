5851c6f6a01c5d23cd7dd4e558a7b02f203768d7
lakshaykapoor03 <kapoorlakshay70@gmail.com>
2022-12-21 18:58:37 +0530

    Added Favourites page to App

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
   const[timeoutId, setTimeoutId] = useState();
   const[movieList, setMovieList] = useState([]);
   const[selectedMovie, setSelectedMovie] = useState();
+  const[favourite, setFavourite] = useState(false);
+  const[addToList, setAddToList] = useState([]);
 
   const fetchData= async(searchString)=>{
     const response= await axios.get(`https://www.omdbapi.com/?s=${searchString}&apikey=${APIKEY}`)
@@ -24,12 +26,18 @@ const timeout=setTimeout(()=>fetchData(event.target.value),500)
 setTimeoutId(timeout)
   };
 
+  const onFavouriteChange=(event)=>{
+    setFavourite(!favourite)
+  }
+
   return (
     <>
   <div className="text-[40px] bg-[black] flex flex-wrap justify-between py-[20px] items-center px-[2%]">
     <div className="flex">
       <img className="w-[8%]" src="https://raw.githubusercontent.com/ayushkul/react-movie-app/cc32c62558a8d0ee01f7db2b7b28589bf8dfbf89/public/react-movie-app/movie-icon.svg"></img>
       <h2 className="text-white">React Movie App</h2>
+      {!favourite ? <button onClick={onFavouriteChange} className="text-[white] text-[20px] bg-[green] rounded-[15px] ml-[10%] h-[40px] w-[120px] mt-[2%]">Favourites</button>
+      :<button onClick={onFavouriteChange} className="text-[white] text-[20px] bg-[green] rounded-[15px] ml-[10%] h-[40px] w-[120px] mt-[2%]">Home</button>}
     </div>
     <div className="w-[800px] h-[40px] bg-white rounded-[20px] flex items-center">
     <i className="fa-solid fa-magnifying-glass text-[20px] mx-5"></i>
@@ -43,9 +51,10 @@ setTimeoutId(timeout)
 
 {/* Hero section */}
 
-{selectedMovie && <MovieInfoComponent selectedMovie={selectedMovie}/>}
+{selectedMovie && !favourite && <MovieInfoComponent setSelectedMovie={setSelectedMovie} addToList={addToList} selectedMovie={selectedMovie} setAddToList={setAddToList}/>}
   <div className="flex flex-wrap justify-evenly mt-[30px] gap-[20px] cursor-pointer">
-{movieList?.length?movieList.map((movie,index)=><MovieComponent movie={movie} key={index} setSelectedMovie={setSelectedMovie}/>):"No Movie Search" }
+{!favourite && movieList?.length?movieList.map((movie,index)=><MovieComponent movie={movie} key={index} setSelectedMovie={setSelectedMovie}/>):"" }
+{favourite && addToList?.length?addToList.map((movie,index)=><MovieComponent movie={movie} key={index} />):"No Movie Search" }
 
   </div>
   </>
--- src/Components/MovieInfoComponent.js
+++ src/Components/MovieInfoComponent.js
@@ -1,14 +1,81 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+const APIKEY = "6be39682";
 
-function MovieInfoComponent() {
-  return (
-  <div className="">
-    <img className="w-[200px] h-[300px] object-cover mx-auto" src="https://m.media-amazon.com/images/M/MV5BMTY0NDY3MDMxN15BMl5BanBnXkFtZTcwOTM5NzMzOQ@@._V1_SX300.jpg" alt="movie info" />
-<h2>Movie Name: </h2>
+const MovieInfoComponent = (props) => {
+  const [movieInfo, setMovieInfo] = useState();
+  
 
+  const addToListHandler = async() => {
+    await props.setAddToList([...props.addToList, movieInfo])
     
+  }
+  // console.log(addToList)
+
+  const { selectedMovie, setSelectedMovie } = props;
+  useEffect(() => {
+    axios
+      .get(`https://www.omdbapi.com/?i=${selectedMovie}&apikey=${APIKEY}`)
+      .then((response) => setMovieInfo(response.data));
+  }, [selectedMovie]);
+  return (
+    <div className="flex justify-between m-[20px] border-b-[3px]">
+      {movieInfo ? (
+        <>
+          <div className="flex m-[20px]">
+            <img
+              className="w-[200px] h-[300px] object-cover"
+              src={movieInfo?.Poster}
+              alt="movie info"
+            />
+            <span className="leading-[35px] ml-[2%]">
+              <h1 className="text-[25px]">Movie:{movieInfo?.Title} </h1>
+              <h3>
+                IMDB Rating:
+                <span className="opacity-50"> {movieInfo?.imdbRating}</span>{" "}
+              </h3>
+              <h3>
+                Year:<span className="opacity-50"> {movieInfo?.Year}</span>{" "}
+              </h3>
+              <h3>
+                Language:
+                <span className="opacity-50"> {movieInfo?.Language}</span>{" "}
+              </h3>
+              <h3>
+                Released:
+                <span className="opacity-50"> {movieInfo?.Released}</span>{" "}
+              </h3>
+              <h3>
+                Runtime:
+                <span className="opacity-50"> {movieInfo?.Runtime}</span>{" "}
+              </h3>
+              <h3>
+                Director:
+                <span className="opacity-50"> {movieInfo?.Director}</span>{" "}
+              </h3>
+              <h3>
+                Plot:<span className="opacity-50"> {movieInfo?.Plot}</span>{" "}
+              </h3>
+            </span>
+          </div>
+          <div className="flex">
+            <div onClick={()=>addToListHandler()}>
+            <i className="fa-solid fa-plus text-[30px] bg-[#ffd51e] pt-[4px] h-[40px] w-[40px] mr-[10px] rounded-[50%] text-center text-black cursor-pointer"></i>
+            </div>
+          <div
+            onClick={() => setSelectedMovie()}
+            className="text-[30px] bg-[#4d4d4d] h-[40px] w-[40px] rounded-[50%] text-center text-white cursor-pointer"
+          >
+            X
+          </div>
+          </div>
+        </>
+      ) : ("Loading..."
+        
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default MovieInfoComponent;
